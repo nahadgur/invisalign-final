6e4b4f6d7c66496c01b12404d0b3392245ad06c1
Jerwin Manongsong <61395841+nahadgur@users.noreply.github.com>
2026-02-12 23:04:53 +0800

    Update page.tsx

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -141,8 +141,8 @@ export default function ArticlePage() {
 
   return (
     <div className="min-h-screen flex flex-col">
-      <Navigation />
-
+      <Navigation onOpenModal={() => setIsModalOpen(true)} />
+      <LeadFormModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
       <main className="flex-1">
         <div className="max-w-5xl mx-auto px-4 py-10">
           {!article ? (
@@ -215,3 +215,4 @@ export default function ArticlePage() {
     </div>
   );
 }
+
